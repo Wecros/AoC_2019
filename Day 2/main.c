7ee5a9918d6c392d5399cf2d94c6e6db36311436
Marek Filip <Wecros>
2019-12-02 13:28:06 +0100

    first part done

---
+++ Day 2/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void intcode_program(int[], int);
+
+int main() {
+    // int c;
+    // char* number = "";
+    // int* arr;
+    // int i = 0;
+    // while ((c = fgetc(stdin)) != EOF) {
+    //     if (c != ',') {
+    //         strncat(number, &c, 1);
+    //     } else {
+    //         printf("num: %s\n", number);
+    //         number = "";
+    //     }
+    // }
+
+    int intcode_arr[] = {1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,1,6,19,23,2,23,6,27,2,6,27,31,2,13,31,35,1,10,35,39,2,39,13,43,1,43,13,47,1,6,47,51,1,10,51,55,2,55,6,59,1,5,59,63,2,9,63,67,1,6,67,71,2,9,71,75,1,6,75,79,2,79,13,83,1,83,10,87,1,13,87,91,1,91,10,95,2,9,95,99,1,5,99,103,2,10,103,107,1,107,2,111,1,111,5,0,99,2,14,0,0};
+
+    int element_count = (int) sizeof(intcode_arr) / sizeof(intcode_arr[0]);
+    intcode_program(intcode_arr, element_count); 
+
+    printf("New array: ");  
+    for (int i = 0; i < element_count; i++) {
+        if (i == element_count - 1) {
+            printf("%d", intcode_arr[i]);
+        } else {
+            printf("%d,", intcode_arr[i]);
+        }
+    }
+
+    printf("\n");
+    return EXIT_SUCCESS;
+}
+
+void intcode_program(int arr[], int element_count) {
+    for (int i = 0; i < element_count; i+=4) {
+        if (arr[i] == 1) {
+            arr[arr[i+3]] = arr[arr[i+1]] + arr[arr[i+2]];
+        } else if (arr[i] == 2) {
+            arr[arr[i+3]] = arr[arr[i+1]] * arr[arr[i+2]];
+        } else if (arr[i] == 99) {
+            break;
+        } else {
+            fprintf(stderr, "ERR: wrong input\n"); 
+            return;
+        }
+    }
+}
