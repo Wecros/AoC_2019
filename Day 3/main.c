636da024c6e20dbfdcb0d646239e8f9d3b029420
Marek Filip <Wecros>
2019-12-03 18:03:30 +0100

    Part 1 done

---
+++ Day 3/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char* firstWire[] = {"R8", "U5", "L5", "D3"};
+    char* secondWire[] = {"U7", "R6", "D4", "L4"};
+
+    
+
+
+    return EXIT_SUCCESS;
+} 
